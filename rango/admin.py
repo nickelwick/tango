de66a9e4a43371fbf5dd11107d2191584df6c572
Nick Elwick <contact@nickelwick.co.uk>
2017-11-12 13:22:27 +0000

    Added extra functionality to the admin page

--- rango/admin.py
+++ rango/admin.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
